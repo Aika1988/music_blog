7d94a558d4d47e8d86a097ea035235cf20a2b0bc
Aika1988 <dzhumagulovaaika88@icloud.com>
2023-02-06 15:44:52 +0600

    second

--- main/urls.py
+++ main/urls.py
@@ -9,5 +9,10 @@ urlpatterns = [
     path('music/', get_music),
     path('music/<int:id>/', get_song),
     path('music/create/', post_music),
+    path('music/delete/<int:id>/', delete_music),
+    path('music/update/<int:id>/', music_update),
+    
+    # get запрос на классах
+    path('music/class/', MusicView.as_view()),
 ]
 
--- music/views.py
+++ music/views.py
@@ -3,6 +3,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from music.models  import Music
 from music.serializers import MusicSerializer
+from rest_framework import generics
 
 
 @api_view(['GET'])
@@ -35,6 +36,39 @@ def post_music(request):
     serializer.is_valid(raise_exception=True)
     serializer.save()
     return Response(serializer.data)
+
+@api_view(['DELETE'])
+def delete_music(request, id):
+    try:
+        song = Music.objects.get(id=id)
+    except Music.DoesNotExist:
+        return Response('нет такой песни!')
+    song.delete()
+    return Response('Successfully deleted!!')        
+
+
+@api_view(['PUT', 'PATCH'])
+def music_update(request, id):
+    try:
+        song = Music.objects.get(id=id)
+    except Music.DoesNotExist:
+        return Response('Not such a song!!')    
+    if request.method == 'PUT':
+        serializer = MusicSerializer(instance=song, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response('Successfully updated!!')
+    else: # PATCH
+        serializer = MusicSerializer(instance=song, data=request.data, partial=True)
+    serializer.is_valid(raise_exception=True)
+    serializer.save()
+    return Response('Successfully updated!!')   
+
+
+# get запрос на классах
+class MusicView(generics.ListAPIView):
+    queryset = Music.objects.all()
+    serializer_class = MusicSerializer 
 
 
 
